eff07c8eee8d0ce90aa8356157e79d055f596cc7
tjkuehl001 <81441973+tjkuehl001@users.noreply.github.com>
2021-03-29 11:08:58 -0500

    Update commaCode.py

--- commaCode.py
+++ commaCode.py
@@ -9,6 +9,6 @@ def listmaker():
     for l in range(len(list1)-1):  # Print all of the list values except the last one.
         print(list1[l] + ', ', end='')
     try:  # Print the last value of the list with formatting for the end of a sentence.
-        print(list1[len(list1)-1] + '.')
+        print('and ' + list1[len(list1)-1] + '.')
     except:  # If the user passes an empty list, display an error.
         print('Error: The list is empty')
